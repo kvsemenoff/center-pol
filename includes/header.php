65de560b139cba7ec3fb243a2038cf12f83a6749
hatuhov <hatuhov@yandex.ru>
2016-09-26 14:42:40 +0300

    r

--- includes/header.php
+++ includes/header.php
@@ -1,70 +1,46 @@
-<section class="ab-wrap-header">
-	<div class="ab-wrap-menu">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12 col-sm-12 col-xs-12">
-					<ul class="ab-main-menu hidden-xs hidden-sm">
-						<li><a href="/">Главная</a></li>
-						<li><a href="#galery">Галерея</a></li>
-						<li><a href="#akcii">Акции</a></li>
-						<li><a href="#news">Новости</a></li>
-						<li><a href="#plan">План<br> Микройона</a></li>
-						<li><a href="#planirovka">Планировки <br> и цены</a></li>
-						<li><a href="#video">Видео</a></li>
-						<li><a href="#info">Информация</a></li>
-						<li><a href="#project">Проектные <br> декларации</a></li>
-						<li><a href="#how">Как купить<br> квартиру</a></li>
-						<li><a href="#contacts">Контакты</a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="clearfix"></div>
-		</div>
-        <div class="anz-menu hidden-lg hidden-md">
-            <input type="checkbox" id="check_1" class="anz-mobile"/>
-            <label class="anz-menu-ch anz-mobile" for="check_1"><i class="fa fa-bars" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i></label>
-            <div class="anz-perspective">
-                <div class="menu-list-pressed">
-                    <ul class="ab-main-menu">
-						<li><a href="/">Главная</a></li>
-						<li><a href="#galery">Галерея</a></li>
-						<li><a href="#akcii">Акции</a></li>
-						<li><a href="#news">Новости</a></li>
-						<li><a href="#plan">План<br> Микройона</a></li>
-						<li><a href="#planirovka">Планировки <br> и цены</a></li>
-						<li><a href="#video">Видео</a></li>
-						<li><a href="#info">Информация</a></li>
-						<li><a href="#project">Проектные <br> декларации</a></li>
-						<li><a href="#how">Как купить<br> квартиру</a></li>
-						<li><a href="#contacts">Контакты</a></li>
-					</ul>
-                </div>
-            </div>
-        </div>
-	</div>
-
+<section class="ah-head">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 col-sm-12 col-xs-12">
-				
-				<div class="ab-raion">
-					<img class="img-responsive" src="img/ab-raion.png" alt="">
+				<div class="ah-logobox">
+					<img src="img/ah-logo.png" alt="ah-logo">
 				</div>
-				<div class="ab-phone-number">
-					<img class="img-responsive" src="img/ab-number.png" alt="">	
-					<span class="first-number">+7(3822)</span><span class="second-number">75-03-57</span>
+			</div>
+
+			<div class="clearfix"></div>
+
+			<div class="col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-head-table">
+					<span class="ah-head-span">Поделиться:</span>
+					<ul class="ah-head-list">
+						<li>
+							<a href="#">
+								<img src="img/ah-soc1.png" alt="ah-soc1">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="img/ah-soc2.png" alt="ah-soc2">
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<img src="img/ah-soc3.png" alt="ah-soc3">
+							</a>
+						</li>
+					</ul>
 				</div>
-				<div class="ab-button">
-					<a href="#zvonok" name="modal">Заказать звонок</a> 
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-12">
+				<span class="ah-head-span1">Теплые полы и напольные покрытия</span>
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-searchform-box">
+					<form action="/" class="ah-searchform">
+			            <input type="text" name="s" placeholder="Поиск" class="ah-poisk">
+						<input type="submit" value="" class="ah-subpoisk">
+					</form>
 				</div>
-				<div class="ab-people">
-					
-				</div>
-				<div class="ab-green-fon">
-					 <span class="ab-green-fon-txt-1">Квартиры</span><br>
-					 <span class="ab-green-fon-txt-2">с отделкой "под ключ"</span><br>
-					 <span class="ab-green-fon-txt-3">от 55 т.р., за м <sup>2</sup></span>
-				</div>			
 			</div>
 		</div>
 	</div>
