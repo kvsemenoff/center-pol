f5a4fd3cbc4a7377f0fce740aaddba64c79fc946
hatuhov <hatuhov@yandex.ru>
2016-09-26 17:05:35 +0300

    ww

---
+++ includes/footer.php
@@ -0,0 +1,63 @@
+
+<div class="clearfix"></div>
+
+<section class="ah-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-logobox">
+					<a href="/"><img src="img/ah-logofot.png" alt="ah-logofot"></a>
+				</div>
+				<span class="ah-footer-span2">Теплые полы и напольные покрытия</span>
+			</div>
+
+			<div class="clearfix"></div>
+
+			<div class="col-md-3 col-sm-3 col-xs-6">
+				<span class="ah-footer-title">ИНФО</span>
+				<ul class="ah-footer-list1">
+					<li><a href="#">КАЛЬКУЛЯТОРЫ</a></li>
+					<li><a href="#">ВИДЕО</a></li>
+					<li><a href="#">ВОПРОСЫ ЭКСПЕРТУ</a></li>
+					<li><a href="#">ДОКУМЕНТЫ</a></li>
+				</ul>
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-6">
+				<span class="ah-footer-title1">О ПОРТАЛЕ</span>
+				<ul class="ah-footer-list2">
+					<li><a href="#">РЕКЛАМОДАТЕЛЯМ</a></li>
+					<li><a href="#">КОНТАКТЫ</a></li>
+					<li><a href="#">КАТАЛОГ ОРГАНИЗАЦИЙ</a></li>
+					<li><a href="#">КАРТА САЙТА</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 col-sm-3 hidden-xs">
+				<div class="ah-fotform-box">
+					<form action="/" class="ah-fotsearchform">
+			            <input type="text" name="s" placeholder="Поиск" class="ah-poisk">
+						<input type="submit" value="" class="ah-subpoisk">
+					</form>
+					<span class="ah-footer-span3">ПОДЕЛИТЬСЯ</span>
+					<ul class="ah-footer-list3">
+						<li>
+							<a href="#"><img src="img/ah-socfot1.png" alt="ah-socfot1"></a>
+						</li>
+						<li>
+							<a href="#"><img src="img/ah-socfot2.png" alt="ah-socfot2"></a>
+						</li>
+						<li>
+							<a href="#"><img src="img/ah-socfot3.png" alt="ah-socfot3"></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="clearfix"></div>
+
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<span class="ah-footer-span">Копирование материалов возможно только с указанием активной,не закрытой от индексации ссылки на источник</span>
+				<span class="ah-footer-span1">© 2013-2016</span>
+			</div>
+		</div>
+	</div>
+</section>
