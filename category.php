70d0b7f1ed04f51883d4a8a42bddb34a02443a2c
Berzeg07 <berezg86@gmail.com>
2016-09-26 13:43:10 +0300

    26.09

---
+++ category.php
@@ -0,0 +1,23 @@
+﻿<?php require_once('includes/header-top.php'); ?>
+<?php require_once('includes/header.php'); ?>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-sm-12 col-xs-12">
+				<div class="ab-main"></div>
+			</div>
+			<div class="col-md-4 col-sm-12 col-xs-12">
+				<div class="sidebar">
+					ffffffffffffffffffffff
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>	
+	</div>
+</section>			
+
+<?php require_once('includes/footer.php'); ?>	
+	
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -42,6 +42,7 @@
 
 	<?php require_once('includes/header.php'); ?>
 	
+	<?php require_once('includes/header-top.php'); ?>
 	<?php require_once('includes/footer.php'); ?>
 	
 	
