d1781608899358eea21de5990dcd1c20e9cf8476
anzarsh <anzarsh@mail.ru>
2016-09-26 15:38:44 +0300

    sdf

--- includes/az-block1.php
+++ includes/az-block1.php
@@ -1,8 +1,17 @@
-<section>
+<section class="az-block1">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h2><span>теплый пол</span></h2>
+				<h2 class="h2margin1"><span>теплый пол</span></h2>
+			</div>
+			<div class="col-md-12">
+				<ul class="az-menu">
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#"></a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
