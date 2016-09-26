2a88f27d6f0bea01b0eacd196259b3afd4a2ce1a
Berzeg07 <berezg86@gmail.com>
2016-09-26 14:40:31 +0300

    26

--- category.php
+++ category.php
@@ -1,17 +1,63 @@
 ﻿<?php require_once('includes/header-top.php'); ?>
 <?php require_once('includes/header.php'); ?>
+
 <section>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-8 col-sm-12 col-xs-12">
-				<div class="ab-main"></div>
+				<div class="ab-main">dfsgh</div>
 			</div>
 			<div class="col-md-4 col-sm-12 col-xs-12">
 				<div class="sidebar">
-					ffffffffffffffffffffff
+					<h3 class="ab-h3-sidebar">Теплый пол</h3>
+					<h3>Электрический теплый пол</h3>
+					<span>Электрический пленочный пол</span>
+					<span>Электрический кабельный теплый пол</span>
+					<span>Нагревательные маты (термоматы)</span>
+					<span>Терморегуляторы</span>
+
+					<h3>Водяной теплый пол</h3>
+					<span>Коллектор</span>
+					<span>Трубы</span>
+					<span>Термостаты</span>
+
+					<h3>Устройство</h3>
+					<h3>Монтаж, укладка, расчет</h3>
+					<h3>Области применения</h3>
+					<h3>Теплоизоляция и стяжка</h3>
+					<h3>Обзоры цены</h3>
+
+					<h3 class="ab-h3-sidebar">Напольные покрытия</h3>
+					<h3>Бетонный пол</h3>
+					<h3>Виниловое покрытие</h3>
+					<h3>Деревянные полы</h3>
+
+					<span>Инженерная доска</span>
+					<span>Массивная доска</span>
+					<span>Паркет</span>
+					<span>Паркетная доска</span>
+					<span>Пол из досок</span>
+					<span>Пробковый</span>
+					<span>Террасная доска</span>
+					<span>Шпунтованная доска</span>
+
+					<h3>Камень и мрамор</h3>
+					<h3>Ковролин</h3>
+					<h3>Ламинат</h3>
+					<h3>Линолеум</h3>
+					<h3>Наливные полы</h3>
+					<h3>Пластиковый (ПВХ)</h3>
+					<h3>Полы по грунту</h3>
+					<h3>Уличные покрытия</h3>
+
+					<span>Тротуарная плитка</span>
+
+					<h3>Плитка</h3>
+
+					
+					
 				</div>
 			</div>
-			<div class="clearfix"></div>
 		</div>	
 	</div>
 </section>			
