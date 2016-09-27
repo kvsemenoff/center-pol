42237becef4f24664df026d92f2267ba2f590ddb
anzarsh <anzarsh@mail.ru>
2016-09-27 10:30:25 +0300

    sldkfj

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<?php require_once('includes/header-top.php'); ?>
 	<?php require_once('includes/header.php'); ?>
 	<?php require_once('includes/az-block1.php'); ?>
-
+	<?php require_once('includes/az-block2.php'); ?>
 	<?php require_once('includes/footer.php'); ?>
 	
 	
